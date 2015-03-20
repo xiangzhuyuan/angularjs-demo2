3babcfcdf4b0abf60cee75c202907aab483b1a80
xiang zhyuan <xiangzhuyuan@gmail.com>
2015-03-20 17:53:57 +0800

    upadte express 4

--- app.js
+++ app.js
@@ -1,56 +1,66 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express'),
-  routes = require('./routes'),
-  api = require('./routes/api');
+    routes = require('./routes'),
+    api = require('./routes/api');
 
-var app = module.exports = express.createServer();
+var app = express();
+
+var pg = require('pg').native,
+    connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/angularjsdemo1',
+    client,
+    query;
 
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.set('view options', {
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set('view options', {
     layout: false
-  });
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.static(__dirname + '/public'));
-  app.use(app.router);
 });
+var connect        = require('connect')
+var methodOverride = require('method-override')
+var compression = require('compression')
+var bodyParser = require('body-parser')
+var errorHandler = require('errorhandler')
+
+app.use(compression())
+app.use(bodyParser.json())
+
+app.use(methodOverride('X-HTTP-Method'))          // Microsoft
+app.use(methodOverride('X-HTTP-Method-Override')) // Google/GData
+app.use(methodOverride('X-Method-Override'))      // IBM
+
+app.use(express.static(__dirname + '/public'));
 
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+var env = process.env.NODE_ENV || 'development'
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
+if ('development' == env) {
+    app.use(errorHandler({dumpExceptions: true, showStack: true}));
+}
+if ('production' == env) {
+    app.use(errorHandler());
+}
 
 // Routes
-
 app.get('/', routes.index);
 app.get('/partials/:name', routes.partials);
-
-// JSON API
-
 app.get('/api/posts', api.posts);
-
 app.get('/api/post/:id', api.post);
-app.post('/api/post', api.addPost);
-app.put('/api/post/:id', api.editPost);
-app.delete('/api/post/:id', api.deletePost);
 
 // redirect all others to the index (HTML5 history)
 app.get('*', routes.index);
 
+app.post('/api/post', api.addPost);
+app.put('/api/post/:id', api.editPost);
+app.delete('/api/post/:id', api.deletePost);
+
+
 // Start server
 
 app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+    console.log("app started...")
 });
